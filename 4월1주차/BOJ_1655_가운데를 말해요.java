e8919f5c31fa224b63d8ca25513a0095c0cac6f8
colosair99 <colosair@gmail.com>
2026-04-01 10:00:33 +0900

    Submit: 가운데를 말해요 (java)

---
+++ 4월1주차/BOJ_1655_가운데를 말해요.java
@@ -0,0 +1,59 @@
+package baekjoon.newbie.week09.boj1655;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// static field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	// main
+	public static void main(String[] args) throws IOException {
+		// init
+		StringBuilder answer = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		// left : min heap  
+		PriorityQueue<Integer> left = new PriorityQueue<>(Comparator.reverseOrder());
+		left.add(-100_001);
+		
+		// right : max heap
+		PriorityQueue<Integer> right = new PriorityQueue<>();
+		right.add(100_001);
+		
+		// solve
+		for(int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			// 왼쪽 삽입 과정
+			if(left.size() == right.size()) {
+				// 오른쪽 값보다 작거나 같으면 왼쪽 즉시 삽입
+				if(num <= right.peek()) left.add(num);
+				// 큰 경우 오른쪽 값을 왼쪽으로 이동 후 오른쪽에 현재 값 삽입
+				else {
+					left.add(right.poll());
+					right.add(num);
+				}
+			}
+			
+			// 오른쪽 삽입 과정
+			else {
+				// 왼쪽 값보다 크거나 같으면 오른쪽 즉시 삽입
+				if(num >= left.peek()) right.add(num);
+				// 큰 경우 오른쪽 값을 왼쪽으로 이동 후 오른쪽에 현재 값 삽입
+				else {
+					right.add(left.poll());
+					left.add(num);
+				}
+			}
+			
+			// 중앙값 출력
+			answer.append(left.peek()).append("\n");
+		}
+
+		// output
+		System.out.println(answer);
+		br.close();
+	}
+}
